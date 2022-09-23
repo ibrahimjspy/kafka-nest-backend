e7ab397f2647259d403af190d5ddd27fa6dadf5f
ibrahimjspy <ibrahimsajidjs@gmail.com>
2022-09-23 10:52:56 +0500

    Update : description transform update

--- src/transforms/productGeneral.ts
+++ src/transforms/productGeneral.ts
@@ -13,6 +13,6 @@ const productNameValidator = async (productObject) => {
 };
 
 const productDescriptionTransform = (productObject) => {
-  const transformedDescription = `{${productObject.description}}`;
+  const transformedDescription = `{\"time\": 1662995227870, \"blocks\": [{\"id\": \"cqWmV3MIPH\", \"data\": {\"text\": \"${productObject.description}\"}, \"type\": \"paragraph\"}], \"version\": \"2.24.3\"}`;
   productObject.description = transformedDescription;
 };
