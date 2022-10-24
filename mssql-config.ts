5c1c84fff495bb7ddee85b3886e655d347181de1
ibrahimjspy <ibrahimsajidjs@gmail.com>
2022-10-24 17:11:16 +0500

    Integration : sql server connection integration

---
+++ mssql-config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  user: 'sa',
+  password: 'Password!',
+  server: 'localhost',
+  database: 'testDB',
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true,
+  },
+};
